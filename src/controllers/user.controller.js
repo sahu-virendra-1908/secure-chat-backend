d2c894e6d09a06930c5b2a4cd34f686a889a4234
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2026-03-10 07:08:47 +0530

    change serch logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,33 @@
 const User = require("../models/user.model");
 
-exports.searchUser = async(req,res)=>{
+exports.searchUser = async (req, res) => {
+  try {
 
- const {query} = req.query;
+    const { query } = req.query;
 
- const user = await User.findOne({
+    const user = await User.findOne({
+      $and: [
+        {
+          $or: [
+            { email: query },
+            { mobile: query }
+          ]
+        },
+        { _id: { $ne: req.userId } }
+      ]
+    });
 
-  $or:[
-   {email:query},
-   {mobile:query}
-  ]
+    if (!user) {
+      return res.json(null);
+    }
 
- });
+    res.json(user);
 
- res.json(user);
+  } catch (err) {
 
+    res.status(500).json({
+      message: "Server error"
+    });
+
+  }
 };
